01c294959731206dbebc052b8d007d0e96a17073
zaidschouwey98 <zazar981@hotmail.com>
2021-11-26 14:15:38 +0100

    Add data_train file

--- http_server.rb
+++ http_server.rb
@@ -1,34 +1,41 @@
 require 'socket'
-server = TCPServer.new 5678
 
-status = 200
+class HttpServer
 
-loop do
-  session = server.accept
-  request = []
-
-  while (line = session.gets) && (line.chomp.length > 0)
-    request << line.chomp
+  def initialize(ip,port)
+    @server = TCPServer.new port
+    @status = 200
+    @host = ip
   end
 
-  http_method, path, protocol = request[0].split(' ') # Get path 
-
-  case path
-  when "/trains"
-    response_body = ""
-  when "/schedules"
-    response_body = ""
-  else
-    status = 404
+  def start_http_server
+    loop do
+      session = @server.accept
+      request = []
+    
+      while (line = session.gets) && (line.chomp.length > 0)
+        request << line.chomp
+      end
+    
+      http_method, path, protocol = request[0].split(' ') # Get path 
+    
+      case path
+      when "/trains"
+        response_body = ""
+      when "/schedules"
+        response_body = ""
+      else
+        status = 404
+      end
+    
+    
+      session.puts <<-HEREDOC
+    HTTP/1.1 #{status}
+    
+    #{response_body}
+      HEREDOC
+    
+      session.close
+    end
   end
-
-
-  session.puts <<-HEREDOC
-HTTP/1.1 #{status}
-
-#{response_body}
-  HEREDOC
-
-  session.close
 end
-

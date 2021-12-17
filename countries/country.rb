410be3aa75af4b04a8212531cd1c7c7a26e35a6b
gabriel rossier <rossier.gabriel218@gmail.com>
2021-12-17 08:46:25 +0100

    Update project structure

---
+++ countries/country.rb
@@ -0,0 +1,20 @@
+class Country
+    def lines
+        raise "Not implemented"
+    end
+    def trains
+        raise "Not implemented"
+    end
+    def schedules 
+        raise "Not implemented"
+    end
+    def events
+        raise "Not implemented"
+    end
+    def trainsonlines
+        raise "Not implemented"
+    end
+    def structure
+        raise "Not implemented"
+    end
+end
